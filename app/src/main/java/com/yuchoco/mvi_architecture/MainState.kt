3534d54826c047379bcea13377c1a39f9e64eaa0
YuChocopie <rladbwjd341@naver.com>
2020-05-16 05:34:22 +0900

    feat: Add MVI

--- app/src/main/java/com/yuchoco/mvi_architecture/MainState.kt
+++ app/src/main/java/com/yuchoco/mvi_architecture/MainState.kt
@@ -1,2 +1,9 @@
 package com.yuchoco.mvi_architecture
 
+sealed class MainState {
+    object LoadingState : MainState()
+    data class DataState(val data: MainModel) : MainState()
+    data class ErrorState(val data: String) : MainState()
+    data class ConfirmationState(val main: MainModel) : MainState()
+    object FinishState : MainState()
+}
--- app/src/main/java/com/yuchoco/mvi_architecture/MainViewModel.kt
+++ app/src/main/java/com/yuchoco/mvi_architecture/MainViewModel.kt
@@ -8,15 +8,43 @@ class MainViewModel : ViewModel(){
 
     var searchText1: String = ""
     var searchText2: String = ""
+    var mainmodel = MainModel("MainModel")
     init {
-        data.value = MainModel("startMainViewModel")
+        mainmodel.testText = "startMainViewModel"
+        data.value = mainmodel
     }
 
     fun setMultiplcation() {
-
+        mainmodel.testText = searchText1
+        render(MainState.DataState(mainmodel))
     }
 
     fun setDivision(){
+        mainmodel.testText = searchText2
+        render(MainState.ErrorState("error$searchText2"))
 
     }
+
+    fun render(state: MainState) {
+        when (state) {
+            is MainState.DataState -> renderDataState(state)
+            is MainState.LoadingState -> renderLoadingState()
+            is MainState.ErrorState -> renderErrorState(state)
+        }
+    }
+
+    fun renderDataState(state: MainState.DataState) {
+        mainmodel = state.data
+        data.value = mainmodel
+    }
+
+    private fun renderErrorState(state: MainState.ErrorState) {
+        mainmodel.testText = state.data
+        data.value = mainmodel
+    }
+
+    private fun renderLoadingState() {
+    }
+
+
 }
