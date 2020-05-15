21513d2a9fcf5dff0b6f943f89d24915664a2d16
YuChocopie <rladbwjd341@naver.com>
2020-05-16 05:16:47 +0900

    feat: Add layout

--- app/src/main/java/com/yuchoco/mvi_architecture/MainViewModel.kt
+++ app/src/main/java/com/yuchoco/mvi_architecture/MainViewModel.kt
@@ -6,7 +6,17 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel(){
     var data = MutableLiveData<MainModel>()
 
+    var searchText1: String = ""
+    var searchText2: String = ""
     init {
         data.value = MainModel("startMainViewModel")
+    }
+
+    fun setMultiplcation() {
+
+    }
+
+    fun setDivision(){
+
     }
 }
